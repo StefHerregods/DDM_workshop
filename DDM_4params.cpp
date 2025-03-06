cfa4c95be36abe8cb42e9c27c4f51563c6932f78
Yvonne Visser <u0172152@kuleuven.be>
2025-03-06 10:43:01 +0100

    example solutions (including new functions) for fitting own data & extra assignment

---
+++ DDM_4params.cpp
@@ -0,0 +1,88 @@
+// in r: install Rcpp library and ZigguratRCPP library, then source this script and the function becomes available (sourceCpp("DDM_basic.cpp"))
+#include <Rcpp.h>
+// [[Rcpp::depends(RcppZiggurat)]]
+#include <Ziggurat.h>
+using namespace Rcpp;
+static Ziggurat::Ziggurat::Ziggurat zigg;
+
+// [[Rcpp::export]]
+List DDM_4params(double v, double a, double ter, NumericVector CC, double z, int ntrials = 1000, double s = 1, double dt = 0.01) { 
+  
+  // parameter names:
+  // v: drift rate
+  // a: bound
+  // ter: non-decision time
+  // z: starting point
+
+  // constants:
+  // ntrials: number of simulated trials
+  
+  // fixed variables:
+  // s: within trial noise
+  // dt: precision (in seconds)
+
+  // initialize matrices for output
+  NumericMatrix DATA(ntrials,2);
+  NumericMatrix DVs(ntrials,(10/dt)); // initialize matrix with length 10 seconds
+  NumericMatrix time(ntrials, (10/dt)); 
+  
+  // loop over trials
+  for (int i = 0; i < ntrials; i++) {
+    
+    // make drift rate negative for half of the trials
+    // use experimental design to determine negative/positive
+    v = CC[i] * v;
+    
+    // initialize variables
+    int acc = 0;
+    double evidence = z*a; // 0; // add starting point here
+    double t = 0;
+    int t_idx = 0;
+    DVs(i,t_idx) = evidence;
+    
+    // Decisional processing
+    while (evidence < a && evidence >-a){
+      
+      t = t + dt;
+      t_idx = t_idx +1;
+      
+      // After the non-decision time, evidence begins accumulating (plus noise)
+      if (t >= ter) {
+        evidence = evidence + v * dt + s * sqrt(dt) * zigg.norm();
+      } else if (t < ter) {
+        evidence = evidence; 
+      }
+      
+      // Save time and DV only for the first 20 trials and max 3 seconds
+      if((i<20) & (t < 3)){
+      time(i,t_idx) = t;
+      DVs(i,t_idx) = evidence;}
+      
+      // if either boundary is hit, determine accuracy of the decision using drift rate
+      if (evidence >= a){
+        //DVs(i,t_idx) = a;
+        if(v>0) {acc = 1;}
+        if(v<0) {acc = 0;}
+        break;
+      } else if (evidence <= -a) {
+        //DVs(i,t_idx) = -a;
+        if(v>0) {acc = 0;}
+        if(v<0) {acc = 1;}
+        break;
+      }
+    }
+    
+    // save the time and accuracy of the decision
+    DATA(i,0) = t;
+    DATA(i,1) = acc;
+    
+  }
+  
+  // return the DVs, reaction times and accuracy 
+  List return_vars;
+  return_vars["DVs"] = DVs;
+  return_vars["time"] = time;
+  return_vars["Data"] = DATA;
+  
+  return return_vars; 
+}
---
+++ DDM_fit_functions_tutorial_examplesolutions.R
@@ -0,0 +1,140 @@
+# calculating cost of observed and predicted data
+# based on RT as well as probability correct
+# input: observed RT, observed probability correct, predicted RT, predicted probability correct
+# output: cost (chi square)
+
+Cost_ddm <- function(obs_RT, obs_acc, pred_RT, pred_acc, plotting){
+  
+  # define quantiles from observed RTs
+  quantiles_cor <- quantile(obs_RT[obs_acc == 1], probs = c(.1,.3,.5,.7,.9), names = FALSE)
+  quantiles_incor <- quantile(obs_RT[obs_acc == 0], probs = c(.1,.3,.5,.7,.9), names = FALSE)
+
+  if(plotting)
+  {
+    par(mfrow = c(2,1))
+    # plot the distributions of correct and incorrect observations and add the quantiles
+    hist(obs_RT[obs_acc == 1],
+         prob = F, col = "green",
+         breaks = 50,
+         xlab="Reaction time", ylab="Occurence", main = "")
+    abline(v = quantiles_cor, col = "green")
+    hist(obs_RT[obs_acc == 0], , add=TRUE,
+         prob = F, col = "red",
+         breaks = 50, main = "")
+    abline(v = quantiles_incor, col = "red")
+  }
+  
+  # Q12 are the quantiles different for the correct and incorrect trials? Why 
+  # do you think so?
+  
+  # now we use the same quantiles but instead of plotting the observed data, we 
+  # plot the predicted data we made from our set of parameters.
+  if(plotting)
+  {
+  hist(pred_RT[pred_acc == 1],
+       prob = F, col = "green",
+       breaks = 50,
+       xlab="Reaction time", ylab="Occurence",, main = "")
+  abline(v = quantiles_cor, col = "green")
+  hist(pred_RT[pred_acc == 0], , add=TRUE,
+       prob = F, col = "red",
+       breaks = 50, main = "")
+  abline(v = quantiles_incor, col = "red")
+  
+  par(mfrow = c(1,1))
+  }
+  
+  # Q13 compare the histogram with the observed data to the histogram with the 
+  # predicted data. Does the same proportion of trials fall within the quantiles
+  # for both sets of data? If not, how are they different?
+  
+  # now we will compute the number of responses that fall within the quantiles
+  # for both observed and predicted data.
+  
+  # for observed data
+  C_prob <- ecdf(obs_RT[obs_acc == 1]) # get cumulative probability distribution
+  obs_prop_cor <- diff(c(0,C_prob(quantiles_cor),1)) * length(obs_RT[obs_acc == 1]) # sample proportion of probability distribution within quantiles
+
+  C_prob <- ecdf(obs_RT[obs_acc == 0]) # same for observed incorrect
+  obs_prop_incor <- diff(c(0,C_prob(quantiles_cor),1)) * length(obs_RT[obs_acc == 0])
+
+  # for predicted data (note that because these data are generated, we sometimes 
+  # have very little (in)correct trials, which we have to account for by setting 
+  # the proportion to zero. Hence, the if statements)
+  
+  if(sum(pred_acc == 1) > 5){ 
+
+  C_prob <- ecdf(pred_RT[pred_acc == 1])
+  pred_prop_cor <- diff(c(0,C_prob(quantiles_cor),1)) * length(pred_RT[pred_acc == 1])}
+  
+  else{ # less than 5 correct responses: no quantiles possible
+    pred_prop_cor <- c(0,0,0,0,0)}
+
+
+  if(sum(pred_acc == 0) > 5){
+  C_prob <- ecdf(pred_RT[pred_acc == 0]) 
+  pred_prop_incor <- diff(c(0,C_prob(quantiles_cor),1)) * length(pred_RT[pred_acc == 0])}
+  else{ # less than 5 errors: no quantiles possible
+    pred_prop_incor <- c(0,0,0,0,0)}
+  
+  # change 0 proportions to very small number to prevent Inf output
+  pred_prop_cor[pred_prop_cor == 0] <- 0.0001
+  pred_prop_incor[pred_prop_incor == 0] <- 0.0001
+  
+  # Q14 compare obs_prop_cor to pred_prop_cor, which quantile has the highest 
+  # number of trials for both?
+  obs_prop_cor
+  pred_prop_cor
+  
+  # we can see that the number of trials is not equal for the observed and 
+  # predicted data, but how can we quantify how much they differ? For this,
+  # we compute the chi-square, as below:
+
+  # get chi-square measure as cost
+  
+  # compute chi square using both correct and incorrect trials
+  Chi_square <- sum(c((obs_prop_cor - pred_prop_cor)^2 / pred_prop_cor, (obs_prop_incor - pred_prop_incor)^2 / pred_prop_incor), na.rm=TRUE)
+
+  # Q15 how would the chi-square change if the number of (in)correct trials in 
+  # a quantile become more similar?
+  
+  return(Chi_square)
+}
+
+# single iteration of fitting procedure, generate data given set of parameters and calculate cost
+# input: observations (2xntrials matrix, column 1 RT, column 2 accuracy)
+Iterate_fit <- function(params, Observations){
+  
+  names(params) <- c('v', 'a', 'ter')
+  
+  sim_data <-DDM_3params(v = params['v'],a = params['a'],ter=params['ter'])
+  
+  cost<-Cost_ddm(Observations[ ,1], Observations[ ,2], sim_data$Data[ ,1], sim_data$Data[ ,2],0)
+  
+  return(cost)
+  
+}
+
+Iterate_fit_withData <- function(params, Observations, CC){
+  
+  names(params) <- c('v', 'a', 'ter')
+  
+  sim_data <-DDM_3params_withData(v = params['v'],a = params['a'],ter=params['ter'], CC)
+  
+  cost<-Cost_ddm(Observations[ ,1], Observations[ ,2], sim_data$Data[ ,1], sim_data$Data[ ,2],0)
+  
+  return(cost)
+  
+}
+
+Iterate_fit_4params <- function(params, Observations, CC){
+  
+  names(params) <- c('v', 'a', 'ter', 'z')
+  
+  sim_data <-DDM_4params(v = params['v'],a = params['a'],ter=params['ter'],z=params['z'],CC)
+  
+  cost<-Cost_ddm(Observations[ ,1], Observations[ ,2], sim_data$Data[ ,1], sim_data$Data[ ,2],0)
+  
+  return(cost)
+  
+}
